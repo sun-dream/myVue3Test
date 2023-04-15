4df2ae978f2e5b4222a6b81fe8e0e91112f391f7
1245595385@qq.com <1245595385@qq.com>
2023-04-15 11:31:34 +0800

    updte

---
+++ test1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 5em;
+  }
+</style>
---
+++ test1/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+   <div>
+     <h2> Filter LearnVue Articles </h2>
+     <input 
+      type='text' 
+      placeholder='Filter Search' 
+      v-model='query'
+    />
+    <br>
+    <button @click='reset'> Reset </button>
+    <search-results :query='query'/>
+   </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import SearchResults from './SearchResults.vue'
+  export default {
+    components: {
+      SearchResults
+    },
+    setup () {
+      const query = ref('')
+
+      const reset = (evt) => {
+        query.value = '' // clears the query
+      }
+      
+      return {
+        reset,
+        query
+      }
+    }
+  }
+</script>
+
+<style>
+  input[type='text'] {
+    margin-bottom: 20px;
+    padding: 10px;
+  }
+</style>
